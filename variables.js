8ff49b1084b7eea894bf2291452ff676affc0b31
Elsie866 <eknabwire@gmail.com>
2025-02-25 23:12:15 -0900

    variables in js

---
+++ variables.js
@@ -0,0 +1,18 @@
+// Variables allow storing data in memory for use in a program/software
+// In js we create/ initialize variables using var,const and let keybwords
+
+// var -- old/legacy way of creating variables
+var age = 90;
+
+// let and const are modern/es6 ways of creating variables
+let username = "Albert";
+const interestRate = 20;
+
+// let is used with vaalues/variables that change as the program runs
+// const is used for values that do not change at any point in the life of a program/software e.g PI
+const PI = 3.142;
+
+//the value in variables can be access in the appllilcation using the variable names
+// console.log(something) is a way for developers to print/see outcome on the terminial/console.
+console.log(age);
+console.log(PI + interestRate);
